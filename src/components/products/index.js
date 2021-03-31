6259d0a35e6770b73eaa812b0ebddfb86a15072d
oscarCorb <hello@oscarcorb.com>
2021-03-31 10:57:05 +0200

    refactor

---
+++ src/components/products/index.js
@@ -0,0 +1,3 @@
+export { default as CreateNewProductPage } from './CreateNewProductPage';
+export { default as ProudctDetailPage } from './ProudctDetailPage';
+export { default as ProudctListPage } from './ProudctListPage';
