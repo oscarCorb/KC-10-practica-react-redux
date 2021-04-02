df70531a2b6d6137d9493bd01942a5945f3e9019
oscarCorb <info@oscarcorb.com>
2021-04-02 13:07:25 +0200

    feat: nav bar menu

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
     <div className="header">
+      <div className="nav-bar">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/adverts">Home</Link>
+            </li>
+            <li>
+              <Link to="/advert/new">New</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <h1>Nodepop React</h1>
     </div>
   );
