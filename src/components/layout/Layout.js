123893b3a42cf319c4a997b23955dae432bbc26b
oscarCorb <info@oscarcorb.com>
2021-04-02 10:20:16 +0200

    tags component

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import CreateNewProductPage from '../products/CreateNewProductPage';
+import LoginPage from '../auth/LoginPage';
 
 const Layout = () => {
   return (
     <>
+      {/* <LoginPage /> */}
       <CreateNewProductPage />
     </>
   );
