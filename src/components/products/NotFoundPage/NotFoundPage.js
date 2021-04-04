cbab9f8d1cff97f51904b5a9e88db2dec1ef9784
oscarCorb <info@oscarcorb.com>
2021-04-04 10:46:47 +0200

    fix: added <Layout /> to NotFoundPage

--- src/components/products/NotFoundPage/NotFoundPage.js
+++ src/components/products/NotFoundPage/NotFoundPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Layout from '../../layout/Layout';
 
 const NotFoundPage = () => {
   return (
     <div className="not-found">
+      <Layout></Layout>
       <p>Error 404</p>
       <p>Página no encontrada</p>
     </div>
