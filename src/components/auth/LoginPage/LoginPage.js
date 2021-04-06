60b992ca5f3b0dce1db80ae3180c054fbeeade62
oscarCorb <info@oscarcorb.com>
2021-04-06 21:27:00 +0200

    fix: success login redirect to home

--- src/components/auth/LoginPage/LoginPage.js
+++ src/components/auth/LoginPage/LoginPage.js
@@ -2,13 +2,18 @@ import React from 'react';
 import LoginForm from './LoginForm';
 import { login } from '../../../api/auth';
 import Layout from '../../layout/Layout';
+import { useHistory } from 'react-router';
 
 const LoginPage = () => {
+  const history = useHistory();
+
   const handleSubmit = async (credentials) => {
     try {
       await login(credentials);
+      history.push('/');
     } catch (error) {
       console.error(error);
+      // TODO: Mostrar error al usuario, ahora no queda claro
     }
   };
 
