1b8ea96a06ce936e7ec61f4c20d55df1d9ef0061
oscarCorb <info@oscarcorb.com>
2021-04-14 19:34:05 +0200

    feat: radiogroup component

---
+++ src/components/shared/RadioGroup.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const RadioGroup = (props) => {
+  return (
+    <div>
+      {props.options.map((item) => (
+        <label key={item.value}>
+          <input
+            // name="sale"
+            type="radio"
+            value={item.value}
+            checked={item.value === props.value}
+            name={props.name}
+            onChange={props.onChange}
+          />
+          {item.label}
+        </label>
+      ))}
+    </div>
+  );
+};
+
+export default RadioGroup;
